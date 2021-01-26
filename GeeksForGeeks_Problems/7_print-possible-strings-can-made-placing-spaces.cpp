c9e73341963fda849c196af381db1a2b8cab40ce
Chandan Puggal <30355904+cpuggal@users.noreply.github.com>
2021-01-27 00:25:31 +0530

    changing code

--- GeeksForGeeks_Problems/7_print-possible-strings-can-made-placing-spaces.cpp
+++ GeeksForGeeks_Problems/7_print-possible-strings-can-made-placing-spaces.cpp
@@ -1,3 +1,6 @@
+//https://www.geeksforgeeks.org/position-of-rightmost-set-bit/
+#include <iostream>
+#include <bits/stdc++.h>
 // Geeksforgeeks : https://www.geeksforgeeks.org/print-possible-strings-can-made-placing-spaces/
 // Given a string you need to print all possible strings that can be made by placing spaces (zero or one) in between them.
 
@@ -7,39 +10,45 @@
 //        A BC
 //        A B C
 
-
-#include<iostream>
-#include "/Users/chandan.p/Documents/Chandan/Chandan-Git/stdc++.h"
-
 using namespace std;
 
 // i = str index
 // j = buf index
-void printer(char *str, char *buf, int len, int i, int j)
+
+
+void printer(string &str, string buf, int len, int i, int j)
 {
-    if (i == len)
-    {
+    if (i == len) {
         // String is over
-        buf[j] = '\0';
+        //buf.c_str();
         cout << buf << endl;
         return;
     }
+    if ((i > len) || (j > len))
+		return;
 // two possibilies, consider char or add space and char
 
-  buf[j] 
+	//consider char
+  buf[j] = str[i];
+  printer(str, buf, len, i + 1, j + 1);
 
+	// add space
+  buf[j] = ' ';
+  buf[j + 1] = str[i];
+  printer(str, buf, len, i + 1, j + 2);
+}
 
-
-void printPossibilities(String str)
+void printPossibilities(string &str)
 {
-    int len = strlen(str);
+    int len = str.length();
 
     if (len == 0)
         cout << "String is empty" << endl;
     else
     {
-        char buf[2*len];
+        string buf(2*len, ' '); // Mistake here
         buf[0] = str[0];
+        //cout << buf << " " << endl;
         printer(str, buf, len, 1, 1);
     }
 }
@@ -47,7 +56,7 @@ void printPossibilities(String str)
 
 int main()
 {
-    char *str = "Chand";
+    string str = "Chand";
     printPossibilities(str);
     return 0;
 }
